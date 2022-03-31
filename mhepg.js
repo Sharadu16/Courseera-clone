4a17bdb073fb6a01f21121236e72d01e7d15b932
Krutika Kishor Wadhai <krut.wad@gmail.com>
2022-03-31 21:25:28 +0530

    31stlast

---
+++ mhepg.js
@@ -0,0 +1,21 @@
+
+document.getElementById("nav21").addEventListener("click", goToHomepg);
+
+function goToHomepg(){
+    window.location.href="mhmepg.html";
+}
+
+document.getElementById("nav22").addEventListener("click", goToInprocess);
+function  goToInprocess(){
+    window.location.href="InProgress.html";
+}
+
+document.getElementById("nav23").addEventListener("click", goToCompleted);
+function goToCompleted(){
+    window.location.href="completed.html";
+}
+
+// img.addEventListener("click", function()
+// {
+// window.location.href="video.html";
+// })
